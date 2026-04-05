e6d11f3359e6df3ad425efad717d19475127b6f6
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-04-05 20:36:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ data "azurerm_resource_group" "rg" {
 
 data "azurerm_storage_account" "sa" {
   name                = var.storage_account_name
-  resource_group_name = data.azurerm_resource_group.rg2.name
+  resource_group_name = var.resource_group_name_storage
 }
 
 data "azurerm_key_vault" "kv" {
