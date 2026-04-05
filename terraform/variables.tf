12ff329507689ea5366200abd22623526453ac7a
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-04-05 20:15:48 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,34 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
 
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "servicebus_namespace_name" {
+  description = "Service Bus namespace name"
+  type        = string
+}
+
+variable "queue_name" {
+  description = "Queue name for orders"
+  type        = string
+}
+
+variable "key_vault_name" {
+  description = "Key Vault name"
+  type        = string
+}
+
+variable "function_app_name" {
+  description = "Function App name"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Storage account name"
+  type        = string
+}
